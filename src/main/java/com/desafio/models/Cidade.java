0221c682026f96bbd0bb314483906f6e27cdecb3
José Jacinto <jose.moraes_BOLS@compasso.com.br>
2020-07-01 16:59:19 -0300

    Criando pacote Models e as classes Cidade e Cliente

---
+++ src/main/java/com/desafio/models/Cidade.java
@@ -0,0 +1,53 @@
+package com.desafio.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TB_CIDADE")
+public class Cidade implements Serializable {
+	
+	private static final long serialVersionUID = 1l;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String nome;
+	
+	private String estado;
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/desafio/models/Cliente.java
@@ -0,0 +1,83 @@
+package com.desafio.models;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TB_CLIENTE")
+public class Cliente implements Serializable {
+	
+	private static final long serialVersionUID = 1l;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String nome;
+	
+	private String sexo;
+	
+	private Date dataNascimento; 
+	
+	private int idade;
+	
+	private Cidade cidadeMora;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public Cidade getCidadeMora() {
+		return cidadeMora;
+	}
+
+	public void setCidadeMora(Cidade cidadeMora) {
+		this.cidadeMora = cidadeMora;
+	}
+	
+	
+	
+	
+}
