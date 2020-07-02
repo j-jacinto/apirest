a021fbf21b817bfba170f9d45429f3e18213aa5f
José Jacinto <jose.moraes_BOLS@compasso.com.br>
2020-07-02 16:03:53 -0300

    Criando o metodo Delete para Cliente
    criei também a opção Delete por Id

--- src/main/java/com/desafio/apirest/resources/ClienteResource.java
+++ src/main/java/com/desafio/apirest/resources/ClienteResource.java
@@ -3,6 +3,7 @@ package com.desafio.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,6 +39,18 @@ public class ClienteResource {
 	@PostMapping("/cliente")
 	public Cliente salvaCliente(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
+		
+	}
+	
+	@DeleteMapping("/cliente")
+	public void deletaCliente(@RequestBody Cliente cliente) {
+		clienteRepository.delete(cliente);
+		
+	}
+	
+	@DeleteMapping("/cliente/{id}")
+	public void deletaCliente(@PathVariable(value="id") long id){
+		clienteRepository.deleteById(id);
 		
 	}
 	
